1f99eb293a5118f4aa73e466402eb25c85d59c1a
qwertytoki <qwertytoki@gmail.com>
2018-12-06 01:56:23 +0900

    change file structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:workoutholic/main.dart';
+import 'package:workoutholic/screen/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
