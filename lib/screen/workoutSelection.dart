ddc26046a394056150710fa8fc2a4657ca7c53e8
qwertytoki <qwertytoki@gmail.com>
2018-12-14 23:01:34 +0900

    connect with firebase yay

--- lib/screen/workoutSelection.dart
+++ lib/screen/workoutSelection.dart
@@ -11,18 +11,63 @@ class WorkoutSelection extends StatelessWidget {
       appBar: AppBar(
         title: Text("Select Workout"),
       ),
-      body: 
-        Column(
-          children:<Widget>[
-            RaisedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Go back!'),
-            ),
-            Text(selectedDate.toString()),
-          ] 
-        ),
+      body: _buildBody(context),
     );
   }
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('workoutMenu').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+   return ListView(
+     padding: const EdgeInsets.only(top: 20.0),
+     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+   );
+  }
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final record = Record.fromSnapshot(data);
+    return Padding(
+      key: ValueKey(record.workoutNameJa),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.workoutNameJa),
+          // trailing: Text(record.votes.toString()),
+          onTap: () => Firestore.instance.runTransaction((transaction) async {
+            final freshSnapshot = await transaction.get(record.reference);
+            final fresh = Record.fromSnapshot(freshSnapshot);
+
+            // await transaction
+            //     .update(record.reference, {'votes': fresh.votes + 1});
+          }),
+        ),
+      ),
+    );
+  }
+}
+class Record {
+  final String workoutNameJa;
+  // final int votes;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['workoutNameJa'] != null),
+        // assert(map['votes'] != null),
+        workoutNameJa = map['workoutNameJa'];
+        // votes = map['votes'];
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$workoutNameJa>";
 }
