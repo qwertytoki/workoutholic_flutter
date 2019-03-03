40162df674d6e1f7909046441d76ffa777cd8662
qwertytoki <qwertytoki@gmail.com>
2019-03-04 02:06:24 +0800

    #2 tiny change

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  
+
   @override
   State<StatefulWidget> createState() => new _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage>{
 
+  String _email;
+  String _password;
+  
   @override
     Widget build(BuildContext context){
       return new Scaffold(
