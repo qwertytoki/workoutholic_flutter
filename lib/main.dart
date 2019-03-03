6280f03ea9237587e26a6d0d680d554c3d77ce1c
qwertytoki <qwertytoki@gmail.com>
2019-03-04 01:56:35 +0800

    #2 login page inprogress

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
-import 'package:workoutholic/widgets/calendar.dart';
+// import 'package:workoutholic/widgets/calendar.dart';
+import ''
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(new MyApp());
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new Calendar(),
+      title:'Login Page',
+      theme: new ThemeData(
+        primarySwatch:Colors.blue,
+      ),
+      home: new LoginPage()
+      // home: new Calendar(),
     );
   }
 }
---
+++ lib/screen/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+    Widget build(BuildContext context){
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Login'),
+        ),
+        body: new Container(
+          child: new Text('Hello world')
+        )
+      );
+    }  
+}
