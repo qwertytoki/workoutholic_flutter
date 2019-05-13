286e6f0a7ba84bb23faf97e4624e14143e28a3a9
qwertytoki <qwertytoki@gmail.com>
2019-05-14 02:05:13 +0800

    update main for auth

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:date_utils/date_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'auth.dart';
+import 'root_page.dart';
 
 // Example holidays
 final Map<DateTime, List> _holidays = {
@@ -16,20 +18,22 @@ final Map<DateTime, List> _holidays = {
 };
 
 void main() {
+  // runApp(new MyApp());
   initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Table Calendar Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Table Calendar Demo'),
-    );
-  }
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'Table Calendar Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth())
+        // home: MyHomePage(title: 'Table Calendar Demo'),
+      );
+    }
 }
 
 class MyHomePage extends StatefulWidget {
