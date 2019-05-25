e8a47836e786824689daa6cd9ea019dab5a0b97c
qwertytoki <qwertytoki@gmail.com>
2019-05-25 17:54:14 +0800

    fix main pare

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:workoutholic/screen/home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 // import 'package:workoutholic/auth.dart';
 // import 'package:workoutholic/root_page.dart';
 
@@ -13,9 +14,9 @@ import 'package:workoutholic/screen/home_page.dart';
 
 
 void main() {
-  // runApp(new MyApp());
+  runApp(new MyApp());
   //これ何してるんだろう
-  initializeDateFormatting().then((_) => runApp(MyApp()));
+  // initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -28,10 +29,11 @@ class _MyAppState extends State<MyApp>{
   final _googleSignIn = new GoogleSignIn();
   final _auth = FirebaseAuth.instance;
 
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: new Text("Firebase Chat")),
-      body: Container(
+    return new MaterialApp(
+      title: 'login',
+      home: Container(
           child: _user == null ? _buildGoogleSignInButton() : HomePage()),
     );
   }
@@ -55,6 +57,7 @@ class _MyAppState extends State<MyApp>{
       ],
     );
   }
+
   Future<FirebaseUser> _handleGoogleSignIn() async {
     GoogleSignInAccount googleUser = await _googleSignIn.signIn();
     GoogleSignInAuthentication googleAuth = await googleUser.authentication;
