c7d57afd5753cc8fcb3c912eac54414af7809aa4
qwertytoki <qwertytoki@gmail.com>
2019-05-25 17:25:02 +0800

    update main dart more sinple

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 //  Copyright (c) 2019 Aleksander Woźniak
 //  Licensed under Apache License v2.0
 
-import 'package:date_utils/date_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:table_calendar/table_calendar.dart';
-import 'package:workoutholic/auth.dart';
-import 'package:workoutholic/root_page.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:workoutholic/screen/home_page.dart';
+// import 'package:workoutholic/auth.dart';
+// import 'package:workoutholic/root_page.dart';
+
 
 
 
@@ -16,16 +18,51 @@ void main() {
   initializeDateFormatting().then((_) => runApp(MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'Table Calendar Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: new RootPage(auth: new Auth())
-        // home: MyHomePage(title: 'Table Calendar Demo'),
-      );
-    }
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp>{
+  FirebaseUser _user;
+  final _googleSignIn = new GoogleSignIn();
+  final _auth = FirebaseAuth.instance;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: new Text("Firebase Chat")),
+      body: Container(
+          child: _user == null ? _buildGoogleSignInButton() : HomePage()),
+    );
+  }
+  Widget _buildGoogleSignInButton() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+            child: RaisedButton(
+          child: Text("Google Sign In"),
+          onPressed: () {
+            _handleGoogleSignIn().then((user) {
+              setState(() {
+                _user = user;
+              });
+            }).catchError((error) {
+              print(error);
+            });
+          },
+        )),
+      ],
+    );
+  }
+  Future<FirebaseUser> _handleGoogleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = await _auth.signInWithCredential(GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken
+    ));
+    print("signed in " + user.displayName);
+    return user;
+  }
 }
