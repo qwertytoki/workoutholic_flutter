6ea228d775ef03c099e2dc804a5635d68821693a
qwertytoki <qwertytoki@gmail.com>
2019-06-04 20:11:58 +0800

    try to move calendar

--- lib/widgets/calendar.dart
+++ lib/widgets/calendar.dart
@@ -1,64 +1,203 @@
-// import 'package:workoutholic/screen/workoutSetSelect.dart';
-// import 'package:flutter/material.dart';
-// // import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart' show CalendarCarousel, WeekDay;
+import 'package:flutter/material.dart';
+import 'package:date_utils/date_utils.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-// class Calendar extends StatefulWidget {
-//   _MyHomePageState createState() => new _MyHomePageState();
-// }
+final Map<DateTime, List> _holidays = {
+  DateTime(2019, 1, 1): ['New Year\'s Day'],
+  DateTime(2019, 1, 6): ['Epiphany'],
+  DateTime(2019, 2, 14): ['Valentine\'s Day'],
+  DateTime(2019, 4, 21): ['Easter Sunday'],
+  DateTime(2019, 4, 22): ['Easter Monday'],
+};
 
-// class _MyHomePageState extends State<Calendar> {
-//   DateTime _currentDate = DateTime.now();
-//   Map<DateTime, int> _markedDateMap = {
-//     //TODO ここでワークアウトの数だけマークつけたい
-//     DateTime(2018, 9, 20) : 4,
-//     DateTime(2018, 10, 11) : 1,
-//     DateTime(2018, 12, 10) : 2,
-//     DateTime(2019, 1, 10) : 3,
-//   };
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//       appBar: new AppBar(
-//         title: new Text("Your Workout Result"),
-//         actions: <Widget>[
-//           new IconButton(icon: const Icon(Icons.today), onPressed: _goToday ),
-//         ],
-//       ),
-//       body: Container(
-//         margin: EdgeInsets.symmetric(horizontal: 16.0),
-//         child: CalendarCarousel(
-//           onDayPressed: (DateTime date) {
-//             this.setState(() => _currentDate = date);
-//           },
-//           weekendTextStyle: TextStyle(
-//             color: Colors.red,
-//           ),
-//           thisMonthDayBorderColor: Colors.grey,
-//           weekFormat: false,
-//           weekends: [WeekDay.Sunday, WeekDay.Saturday],
-//           markedDatesMap: _markedDateMap,
-//           height: 420.0,
-//           selectedDateTime: _currentDate,
-//           daysHaveCircularBorder: null, /// null for not rendering any border, true for circular border, false for rectangular border
-//         ),
-//       ), 
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _goInputPage,
-//          tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ),// This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
+Widget _buildTableCalendarWithBuilders() {
+  DateTime _selectedDay;
+  Map<DateTime, List> _events;
+  Map<DateTime, List> _visibleEvents;
+  Map<DateTime, List> _visibleHolidays;
+  List _selectedEvents;
+  AnimationController _controller;
+  _selectedDay = DateTime.now();
+    _events = {
+      _selectedDay.subtract(Duration(days: 5)): ['Bench Press', 'Squat'],
+      _selectedDay.subtract(Duration(days: 2)): ['Bench Press', 'Squat'],
+      _selectedDay: ['Bench Press', 'Squat', 'Dead Lift', 'Chinning', 'Plunk'],
+    };
 
-//   void _goToday(){
-//     this.setState(() => _currentDate = new DateTime.now());
-//   }
+    _selectedEvents = _events[_selectedDay] ?? [];
+    _visibleEvents = _events;
+    _visibleHolidays = _holidays;
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
 
-//   void _goInputPage(){
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(builder: (context) => WorkoutSetSelect(selectedDate :_currentDate)),
-//     );
-//   }   
-// }
+    _controller.forward();
+    return TableCalendar(
+      locale: 'en_US',
+      events: _visibleEvents,
+      holidays: _visibleHolidays,
+      initialCalendarFormat: CalendarFormat.month,
+      formatAnimation: FormatAnimation.slide,
+      startingDayOfWeek: StartingDayOfWeek.sunday,
+      availableGestures: AvailableGestures.all,
+      availableCalendarFormats: const {
+        CalendarFormat.month: '',
+        CalendarFormat.week: '',
+      },
+      calendarStyle: CalendarStyle(
+        outsideDaysVisible: true,
+        weekendStyle: TextStyle().copyWith(color: Colors.blue[800]),
+        holidayStyle: TextStyle().copyWith(color: Colors.blue[800]),
+      ),
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekendStyle: TextStyle().copyWith(color: Colors.blue[600]),
+      ),
+      headerStyle: HeaderStyle(
+        centerHeaderTitle: true,
+        formatButtonVisible: false,
+      ),
+      builders: CalendarBuilders(
+        selectedDayBuilder: (context, date, _) {
+          return FadeTransition(
+            opacity: Tween(begin: 0.0, end: 1.0).animate(_controller),
+            child: Container(
+              margin: const EdgeInsets.all(4.0),
+              padding: const EdgeInsets.only(top: 5.0, left: 6.0),
+              color: Colors.deepOrange[300],
+              width: 100,
+              height: 100,
+              child: Text(
+                '${date.day}',
+                style: TextStyle().copyWith(fontSize: 16.0),
+              ),
+            ),
+          );
+        },
+        todayDayBuilder: (context, date, _) {
+          return Container(
+            margin: const EdgeInsets.all(4.0),
+            padding: const EdgeInsets.only(top: 5.0, left: 6.0),
+            color: Colors.amber[400],
+            width: 100,
+            height: 100,
+            child: Text(
+              '${date.day}',
+              style: TextStyle().copyWith(fontSize: 16.0),
+            ),
+          );
+        },
+        markersBuilder: (context, date, events, holidays) {
+          final children = <Widget>[];
+
+          if (events != null) {
+            children.add(
+              Positioned(
+                right: 1,
+                bottom: 1,
+                child: _buildEventsMarker(date, events),
+              ),
+            );
+          }
+
+          if (holidays != null) {
+            children.add(
+              Positioned(
+                right: -2,
+                top: -2,
+                child: _buildHolidaysMarker(),
+              ),
+            );
+          }
+
+          return children;
+        },
+      ),
+      onDaySelected: (date, events) {
+        _onDaySelected(date, events);
+        _controller.forward(from: 0.0);
+      },
+      onVisibleDaysChanged: _onVisibleDaysChanged,
+    );
+    void _onDaySelected(DateTime day, List events) {
+    setState(() {
+      _selectedDay = day;
+      _selectedEvents = events;
+    });
+  }
+
+  void _onVisibleDaysChanged(
+      DateTime first, DateTime last, CalendarFormat format) {
+    setState(() {
+      _visibleEvents = Map.fromEntries(
+        _events.entries.where(
+          (entry) =>
+              entry.key.isAfter(first.subtract(const Duration(days: 1))) &&
+              entry.key.isBefore(last.add(const Duration(days: 1))),
+        ),
+      );
+
+      _visibleHolidays = Map.fromEntries(
+        _holidays.entries.where(
+          (entry) =>
+              entry.key.isAfter(first.subtract(const Duration(days: 1))) &&
+              entry.key.isBefore(last.add(const Duration(days: 1))),
+        ),
+      );
+    });
+  }
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: Utils.isSameDay(date, _selectedDay)
+            ? Colors.brown[500]
+            : Utils.isSameDay(date, DateTime.now())
+                ? Colors.brown[300]
+                : Colors.blue[400],
+      ),
+      width: 16.0,
+      height: 16.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHolidaysMarker() {
+    return Icon(
+      Icons.add_box,
+      size: 20.0,
+      color: Colors.blueGrey[800],
+    );
+  }
+
+  Widget _buildEventList() {
+    return ListView(
+      children: _selectedEvents
+          .map((event) => Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.8),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                child: ListTile(
+                  title: Text(event.toString()),
+                  onTap: () => print('$event tapped!'),
+                ),
+              ))
+          .toList(),
+    );
+    
+  }
+  
