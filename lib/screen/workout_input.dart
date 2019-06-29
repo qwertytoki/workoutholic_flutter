7d081472a7ad87a751115c6ffe3d24da9a6665a3
qwertytoki <qwertytoki@gmail.com>
2019-06-29 21:20:36 +0900

    comment unused

--- lib/screen/workout_input.dart
+++ lib/screen/workout_input.dart
@@ -49,40 +49,6 @@ class _WorkoutInputPageState extends State<WorkoutInputPage> {
           }),
     );
   }
-
-    // return StreamBuilder<QuerySnapshot>(
-    //     stream: WorkoutMenu.getMenuFromWorkoutSet(_workoutSet.workoutIds),
-    //     builder: (context, snapshot) {
-    //       if (!snapshot.hasData) return LinearProgressIndicator();
-    //       return _buildList(context, snapshot.data.documents);
-    //     });
-  }
-
-  // for firebase
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
-    final workoutSet = WorkoutSet.fromSnapshot(data);
-    return Padding(
-      key: ValueKey(workoutSet.setName),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(workoutSet.setName),
-        ),
-      ),
-    );
-  }
-
   showPicker(BuildContext context) {
     Picker picker = new Picker(
         adapter: PickerDataAdapter<String>(
@@ -96,4 +62,75 @@ class _WorkoutInputPageState extends State<WorkoutInputPage> {
         });
     picker.show(_scaffoldKey.currentState);
   }
-}
+
+  // Widget _buildBody(BuildContext context) {
+  //   return ListView.builder(
+  //     itemCount: items.length,
+  //     itemBuilder: (context, index) {
+  //       final item = items[index];
+  //       return Dismissible(
+  //         key: Key(item),
+  //         onDismissed: (direction) {
+  //           setState(() {
+  //             items.removeAt(index);
+  //           });
+  //           Scaffold.of(context)
+  //               .showSnackBar(SnackBar(content: Text("deleteする")));
+  //         },
+  //         background: Container(color: Colors.red),
+  //         child: ListTile(
+  //           title: Text('$item'),
+  //           onTap: () {
+  //             // showDialog(
+  //             //       context: context,
+  //             //       builder: (BuildContext context) {
+  //             //         return AlertDialog(
+  //             //           title: Text("test"),
+  //             //         );
+  //             //       }
+  //             //   );
+  //             //うまく動かない
+  //             showPicker(context);
+  //           },
+  //         ),
+  //       );
+  //     },
+  //   );
+    
+
+    // return StreamBuilder<QuerySnapshot>(
+    //     stream: WorkoutMenu.getMenuFromWorkoutSet(_workoutSet.workoutIds),
+    //     builder: (context, snapshot) {
+    //       if (!snapshot.hasData) return LinearProgressIndicator();
+    //       return _buildList(context, snapshot.data.documents);
+    //     });
+  }
+  
+
+  // // for firebase
+  // Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  //   return ListView(
+  //     padding: const EdgeInsets.only(top: 20.0),
+  //     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+  //   );
+  // }
+
+  // Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+  //   final workoutSet = WorkoutSet.fromSnapshot(data);
+  //   return Padding(
+  //     key: ValueKey(workoutSet.setName),
+  //     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+  //     child: Container(
+  //       decoration: BoxDecoration(
+  //         border: Border.all(color: Colors.grey),
+  //         borderRadius: BorderRadius.circular(5.0),
+  //       ),
+  //       child: ListTile(
+  //         title: Text(workoutSet.setName),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  
+
