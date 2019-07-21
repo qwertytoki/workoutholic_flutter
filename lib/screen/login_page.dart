1f25bae668cf7a22fa28ed07d1583895947d8c5c
qwertytoki <qwertytoki@gmail.com>
2019-07-22 03:47:14 +0800

    login by firebase

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -4,6 +4,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:workoutholic/screen/home_page.dart';
 import 'package:workoutholic/dao/user_dao.dart';
+import 'package:workoutholic/dto/user.dart';
 
 class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -36,11 +37,12 @@ void _handleGoogleSignIn(BuildContext context) async {
   final _auth = FirebaseAuth.instance;
   GoogleSignInAccount googleUser = await _googleSignIn.signIn();
   GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-  FirebaseUser user = await _auth.signInWithCredential(
+  FirebaseUser fbUser = await _auth.signInWithCredential(
       GoogleAuthProvider.getCredential(
           accessToken: googleAuth.accessToken, idToken: googleAuth.idToken));
+  User user = await UserDao.getUserByUid(fbUser.uid);
   print("signed in " + user.displayName);
-  if (user != null) {
+  if (fbUser != null) {
     Navigator.of(context).pushReplacement(MaterialPageRoute(
       builder: (context) {
         return HomePage();
