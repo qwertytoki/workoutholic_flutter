c91d2703de0d62d703ade26ac048901d357f2b6b
qwertytoki <qwertytoki@gmail.com>
2019-07-23 03:12:06 +0800

    get work log

--- lib/dao/work_log_dao.dart
+++ lib/dao/work_log_dao.dart
@@ -1,5 +1,11 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:workoutholic/dto/work_log.dart';
 
 class WorkLogDao {
-  
+  static Stream<QuerySnapshot> getLogByUserId(String userId) {
+    return Firestore.instance
+        .collection("workLog")
+        .where('user_id', isEqualTo: userId)
+        .snapshots();
+  }
 }
