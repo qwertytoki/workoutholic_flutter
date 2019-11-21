9d1d003cdaf9b1b865148054da48ed7907c4fb4f
qwertytoki <qwertytoki@gmail.com>
2019-11-22 00:14:47 +0800

    delete unused method

--- lib/dao/latest_work_log_dao.dart
+++ lib/dao/latest_work_log_dao.dart
@@ -3,27 +3,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:workoutholic/dto/latest_work_log.dart';
 
 class LatestWorkLogDao {
-  static Future<List<LatestWorkLog>> getLogByUserAndDate(
-      String userId, DateTime date) async {
-    List<LatestWorkLog> list = new List();
-    date = new DateTime(date.year, date.month, date.day);
+  static Future<List<LatestWorkLog>> getLogByCodes(
+      List<String> menuCodeList) async {
     QuerySnapshot snapshot = await Firestore.instance
         .collection("latestWorkLog")
-        .where('user_id', isEqualTo: userId)
-        .where('date', isEqualTo: date)
+        .where('menu_code', arrayContains: menuCodeList)
         .getDocuments();
-    snapshot.documents.forEach((s) {
-      list.add(LatestWorkLog.of(s));
-    });
-    return list;
-  }
-  static Future<List<LatestWorkLog>> getLogByCodes(List<String> menuCodeList)async{
-    QuerySnapshot snapshot = await Firestore.instance
-      .collection("latestWorkLog")
-      .where('menu_code',arrayContains: menuCodeList)
-      .getDocuments();
     List<LatestWorkLog> list = new List();
-    snapshot.documents.forEach((s){
+    snapshot.documents.forEach((s) {
       list.add(LatestWorkLog.of(s));
     });
     return list;
