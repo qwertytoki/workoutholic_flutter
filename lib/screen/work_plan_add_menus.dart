1a6ca73a9c19db695dd71eeb34b20ba5a1336648
qwertytoki <qwertytoki@gmail.com>
2019-11-27 01:48:03 +0800

    #10 prepare work menu add page

--- lib/screen/work_plan_add_menus.dart
+++ lib/screen/work_plan_add_menus.dart
@@ -14,5 +14,31 @@ class WorkPlanAddMenuPage extends StatefulWidget{
 }
 
 class _WorkPlanAddMenuState extends State<WorkPlanAddMenuPage>{
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  @override 
+  void initState(){
+    super.initState();
+  }
+  Widget build(BuildContext context){
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: _buildAppBar(context),
+      body: _buildBody(context)
 
+    );
+  }
+  Widget _buildAppBar(BuildContext context){
+    return AppBar(title: Text("追加するメニューを選択"),);
+  }
+  Widget _buildBody(BuildContext context){
+    return ListTile(
+                title: Text("完了", textAlign: TextAlign.center),
+                onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => WorkPlanCreatePage(
+                            user: widget.user, date: widget.date),
+                      ),
+                    ));
+  }
 }
